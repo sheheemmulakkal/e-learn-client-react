06f1d26cafb6d3ba2bcf05ddc8485a71d378f029
sheheemmulakkal <shaimonsheheem@gmail.com>
2023-12-18 13:48:05 +0530

    style: Add icons to dashboard
    This commit enhances the dashboard by incorporating icons for improved visual representation. The addition of icons enhances the overall user interface.

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -35,30 +35,48 @@ function AdminDashboard() {
             </h1>
             <LineChart />
           </div>
-          <div className="flex flex-col shadow-lg p-5 border  text-black rounded-md">
-            <div className="container grid grid-cols-2 gap-3">
-              <div className="rounded-lg h-36 flex flex-col bg-[#bcffc7] gap-4 px-5 justify-center items-center shadow-md border">
-                <h1 className="font-bold text-lg">
-                  Total students: {data?.studentCount}
-                </h1>
+          <div className="flex flex-col shadow-lg p-5 border h-full items-center  text-black rounded-md">
+            <div className="container grid grid-cols-2 items-center gap-3">
+              <div className="rounded-lg h-36 flex flex-col bg-[#dcffe2] gap-4 px-5 justify-center items-center shadow-md border">
+                <div className="flex flex-row items-center gap-3">
+                  <i
+                    className="fa-solid fa-graduation-cap text-xl"
+                    style={{ color: "#1f4551" }}
+                  ></i>
+                  <h1 className="font-bold text-lg">
+                    Total students: {data?.studentCount}
+                  </h1>
+                </div>
                 <p className="text-center text-sm italic">
                   This is the count of total active students signed up.
                 </p>
               </div>
 
-              <div className="rounded-lg h-36 flex bg-[#ffbcbc] flex-col gap-4 px-5 justify-center items-center shadow-md border">
-                <h1 className="font-bold text-lg">
-                  Total instructors: {data?.instructorCount}
-                </h1>
+              <div className="rounded-lg h-36 flex bg-[#fad8d8] flex-col gap-4 px-5 justify-center items-center shadow-md border">
+                <div className="flex flex-row items-center gap-3">
+                  <i
+                    className="fa-solid fa-person-chalkboard text-xl"
+                    style={{ color: "#1f4551" }}
+                  ></i>
+                  <h1 className="font-bold text-lg">
+                    Total instructors: {data?.instructorCount}
+                  </h1>
+                </div>
                 <p className="text-center text-sm italic">
                   This is the total number of active instructors joined so far.
                 </p>
               </div>
 
-              <div className="rounded-lg h-36 flex flex-col bg-[#bcd7ff] gap-4 px-5 justify-center items-center shadow-md border">
-                <h1 className="font-bold text-lg">
-                  Total Courses: {data?.courseCount}
-                </h1>
+              <div className="rounded-lg h-36 flex flex-col bg-[#dae9ff] gap-4 px-5 justify-center items-center shadow-md border">
+                <div className="flex flex-row items-center gap-3">
+                  <i
+                    className="fa-solid fa-book text-xl"
+                    style={{ color: "#1f4551" }}
+                  ></i>
+                  <h1 className="font-bold text-lg">
+                    Total Courses: {data?.courseCount}
+                  </h1>
+                </div>
                 <p className="text-center text-sm italic">
                   This number indicates the total number of courses created by
                   instructor. The count takes only the courses that approved by
@@ -66,10 +84,16 @@ function AdminDashboard() {
                 </p>
               </div>
 
-              <div className="rounded-lg h-36 flex flex-col bg-[#feffbc] gap-4 px-5 justify-center items-center shadow-md border">
-                <h1 className="font-bold text-lg">
-                  Total revenue: {data?.totalRevenue}
-                </h1>
+              <div className="rounded-lg h-36 flex flex-col bg-[#feffd7] gap-4 px-5 justify-center items-center shadow-md border">
+                <div className="flex flex-row items-center gap-3">
+                  <i
+                    className="fa-solid fa-chart-line text-xl"
+                    style={{ color: "#1f4551" }}
+                  ></i>
+                  <h1 className="font-bold text-lg">
+                    Total revenue: {data?.totalRevenue}
+                  </h1>
+                </div>
                 <p className="text-center text-sm italic">
                   The revenue indicates the total amount gained by the website
                   throught the students enrollment.
