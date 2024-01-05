1ed07abf97fc813e350d497afc218a33dfc8830b
sheheemmulakkal <shaimonsheheem@gmail.com>
2024-01-05 12:36:56 +0530

    style: Update logo color
    This commit modifies the color of the logo on the frontend. The change is made for improved visual aesthetics and consistency in the application's design.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -134,9 +134,10 @@ export default function NavBar() {
           <Link to={user?.role === Roles.instructor ? "/instructor" : "/"}>
             <Typography
               as="a"
-              className="mr-4 px-5 text-2xl cursor-pointer py-1.5 font-bold text-black"
+              className="mr-4 px-5 text-2xl cursor-pointer py-1.5 font-bold text-[#2F327D]"
             >
-              EduVista
+              <span className="text-[#F48C06] ">Edu</span>
+              Vista
             </Typography>
           </Link>
           <div className="flex items-center gap-4">
